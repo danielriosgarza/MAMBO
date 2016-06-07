9eabe81fcb60b4db71ebb41a986d51a082a49f53
Daniel Garza <danielriosgarza@gmail.com>
2016-06-07 13:44:50 +0200

    Create setup_bte_functions.py

---
+++ setup_bte_functions.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
+
+
+import numpy
+
+ext = Extension("functions_cython", ["functions_cython.pyx"],
+    include_dirs = [numpy.get_include()])
+
+
+setup(ext_modules=[ext],
+      cmdclass = {'build_ext': build_ext})
